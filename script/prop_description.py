cd40850967a33a7f487dfd8885e671fc2ab92916
Sylvia-LIN <linqyd@live.com>
2018-10-19 22:42:17 -0700

    Fix some bugs

--- script/prop_description.py
+++ script/prop_description.py
@@ -8,7 +8,7 @@ from pprint import pprint
 import re
 
 
-template_path = ""
+template_path = "../json_template/desc_template.json"
 
 
 def read_args():
@@ -37,14 +37,14 @@ def get_query_result(query_req) -> List[dict]:
 
 
 def get_property_attr_query(property):
-    property_attr_query = \
-        'SELECT DISTINCT ?desc ?prop_label WHERE \
+    property_attr_query = 'select distinct ?desc ?prop_label WHERE \
         {\
           wd:' + property + ' schema:description ?desc.\
           filter (lang(?desc)="en")\
-          wd:P1546 rdfs:label ?prop_label.\
+          wd:' + property + ' rdfs:label ?prop_label.\
           filter (lang(?prop_label)="en")\
         }'
+
     return property_attr_query
 
 
@@ -68,8 +68,9 @@ def get_identifier_attr_query(property):
 
 
 def fill_description(desc_template, data):
+    data = data[0]
     desc_template['title'] = desc_template['title'] + data['prop_label']['value'].upper()
-    desc_template['description'] = data['']
+    desc_template['description'] = data['desc']['value']
     desc_template['url'] = desc_template['url'] + property
     return desc_template
 
@@ -80,17 +81,17 @@ def encode_url(url):
 
 
 def process_property_attr_query(desc_template, data):
-    variables = dict()
+    variables = list()
     for item in data:
+        # print(item)
         # assemble variable
         variable = dict()
         variable['name'] = item['id_l']['value']
         variable['description'] = item['desc']['value']
-        variable.setdefault('semantic_type', [])
-                .append(item['type']['value'])
+        variable.setdefault('semantic_type', []).append(item['type']['value'])
         variable['external_identifier'] = ""
         variable['named_entity'] = list()
-        variable['temporal_coverage']=
+        variable['temporal_coverage']= \
             { 
                 "start": "",
                 "end": ""
@@ -105,26 +106,22 @@ def process_property_attr_query(desc_template, data):
 if __name__ == '__main__':
     property = read_args()
 
-    property_attr_query_encoded = encoded_url(get_property_attr_query)
-    identifier_attr_query_encoded = encoded_url(get_identifier_attr_query)
+    property_attr_query_encoded = encode_url(get_property_attr_query(property))
+    identifier_attr_query_encoded = encode_url(get_identifier_attr_query(property))
 
     prefix = 'https://query.wikidata.org/sparql?query='
     format = '&format=json'
 
-
+    print("property_attr_query ", prefix + property_attr_query_encoded + format)
     property_attr_query = urllib.request.Request(prefix + property_attr_query_encoded + format)
-    
+
+    print("identifier_attr ", prefix + identifier_attr_query_encoded + format)
     identifier_attr_query = urllib.request.Request(prefix + identifier_attr_query_encoded + format)
 
     desc_template = read_template()
 
-    desc_template = process_property_attr_query(desc_template, get_query_result(property_attr_query))
+    desc_template = fill_description(desc_template, get_query_result(property_attr_query))
 
-    desc_template = property_attr_query_encoded(desc_template, get_query_result(identifier_attr_query))
+    desc_template = process_property_attr_query(desc_template, get_query_result(identifier_attr_query))
 
     pprint(desc_template)
-
-
-
-
-
