beb1e4bd73a8cbcea2b66b356280dc3152c6e142
Sylvia-LIN <linqyd@live.com>
2018-10-22 11:30:04 -0700

    add coverage.txt.

--- script/sub_property_count.py
+++ script/sub_property_count.py
@@ -4,12 +4,12 @@ import csv
 import copy
 import json
 from typing import List
-
+from pprint import pprint
 
 main_query = 'select ?prop where{\
-	?prop a owl:ObjectProperty. \
-	filter (regex(str(?prop), "^http://www.wikidata.org/prop/direct/"))\
-	}'
+    ?prop a owl:ObjectProperty. \
+    filter (regex(str(?prop), "^http://www.wikidata.org/prop/direct/"))\
+    }'
 
 encoded_url = urllib.parse.quote(main_query)
 
@@ -24,40 +24,41 @@ id_category_query_req = urllib.request.Request(url)
 
 data = {}
 with urllib.request.urlopen(id_category_query_req) as r:
-	data = json.loads(r.read().decode('utf-8'))
+    data = json.loads(r.read().decode('utf-8'))
 
 result = data['results']['bindings']
 
 properties = list()
 
 for record in result:
-	properties.append(record['prop']['value'])
+    properties.append(record['prop']['value'])
 
 print(len(properties))
 
 count_map = dict()
 
+retrievable, unretrievable = 0, 0
+
 for prop in properties:
-	count_sub_query = 'select (count(distinct ?sub) as ?num) where{\
+    count_sub_query = 'select (count(distinct ?sub) as ?num) where{\
 		?sub <' + prop + '> ?val.\
-	}'
+    }'
+    count_sub_url = prefix + urllib.parse.quote(count_sub_query) + format
+    count_sub_req = urllib.request.Request(count_sub_url)
 
-	count_sub_url = prefix+urllib.parse.quote(count_sub_query)+format
+    count_data = {}
+    try:
+        with urllib.request.urlopen(count_sub_req) as r:
+            count_data = json.loads(r.read().decode('utf-8'))
+            count = int(count_data['results']['bindings'][0]['num']['value'])
+        count_map[prop] = count
+        retrievable += 1
+        print(prop, " ", count_map[prop])
+    except Exception as e:
+        unretrievable += 1
+        continue
 
-	count_sub_req = urllib.request.Request(count_sub_url)
-
-	count_data = {}
-	try:
-		with urllib.request.urlopen(count_sub_req) as r:
-			count_data = json.loads(r.read().decode('utf-8'))
-			count = int(count_data['results']['bindings'][0]['num']['value'])
-
-		count_map[prop] = count
-		print(count_map[prop])
-	except Exception as e:
-		continue
-
-	
 sorted_by_value = sorted(count_map.items(), key=lambda kv: kv[1])
-print(sorted_by_value)
-
+pprint(sorted_by_value)
+print("retrieved: ", retrievable)
+print ("unretrieved: ", unretrievable)
