4df992b370f236a19b2114099769349f22825103
Sylvia-LIN <linqyd@live.com>
2018-10-22 00:49:23 -0700

    add category for entity. fix bugs in script for generating description.json

--- script/sparql.py
+++ script/sparql.py
@@ -7,17 +7,21 @@ from typing import List
 from pprint import pprint
 import re
 
+
 def read_args():
     property = ""
     if len(sys.argv) > 1:
         property = sys.argv[1].strip()
 
     return property
-    
+
+
 def formulate_main_query(property):
-    main_query = 'select distinct ?source ?source_l ?prop_l ?prop_value ?know_as ?id_l ?id ?id_value where{\
+    main_query = 'select distinct ?source ?source_l ?category ?prop_l ?prop_value ?know_as ?id_l ?id ?id_value where{\
         ?source wdt:' + property + ' ?prop_value.\
         ?source rdfs:label ?source_l.\
+        ?source wdt:P31/rdfs:label ?category.\
+        filter (lang(?category)="en")\
         filter (lang(?source_l)="en")\
         wd:' + property + ' rdfs:label ?prop_l.\
         filter (lang(?prop_l)="en")\
@@ -27,6 +31,8 @@ def formulate_main_query(property):
         ?identifier wikibase:directClaim ?id.\
         ?identifier wikibase:propertyType wikibase:ExternalId.\
         ?identifier rdfs:label ?id_l.\
+        ?identifier schema:description ?desc.\
+        filter (lang(?desc)="en")\
         filter (lang(?id_l)="en")\
         }'
 
@@ -39,6 +45,8 @@ def formulate_id_category_query(property):
             ?source wdt:' + property + ' ?value.\
             ?source ?id ?idValue.\
             ?identifier ?ref ?id.\
+            optional {?value rdfs:label ?know_as.\
+            filter (lang(?know_as)="en")}\
             ?identifier wikibase:directClaim ?id.\
             ?identifier wikibase:propertyType wikibase:ExternalId.\
             ?identifier rdfs:label ?l.\
@@ -69,7 +77,7 @@ def process_id_category_query(data):
     ids = dict()
     for item in data:
         identifier = item['l']['value']
-        ids[identifier] = list()
+        ids[identifier] = set()
 
     return ids
 
@@ -79,27 +87,30 @@ def process_main_query(data, ids):
     property_label = ""
 
     for item in data:
-        property_label = item['prop_l']['value']
-        source = item['source']['value']
-        prop_value = item['prop_value']['value']
-        know_as = item['know_as']['value'] if 'know_as' in item.keys() else None
-        subject_l = item['source_l']['value']
-        id = item['id']['value']
-        id_l = item['id_l']['value']
-        id_value = item['id_value']['value']
+        category = item['category']['value'].strip()
+        property_label = item['prop_l']['value'].strip()
+        source = item['source']['value'].strip()
+        prop_value = item['prop_value']['value'].strip()
+        know_as = item['know_as']['value'].strip() if 'know_as' in item.keys() else None
+        subject_l = item['source_l']['value'].strip()
+        id = item['id']['value'].strip()
+        id_l = item['id_l']['value'].strip()
+        id_value = item['id_value']['value'].strip()
 
         if source not in result.keys():
             result[source] = dict()
             result[source]['source'] = source
+            result[source]['category'] = set()
             result[source]['prop_value'] = set()
             result[source]['subject_label'] = set()
             result[source]['value_label'] = set()
             result[source].update(copy.deepcopy(ids))
 
         result[source]['prop_value'].add(prop_value)
+        result[source]['category'].add(category)
         result[source]['subject_label'].add(subject_l)
         result[source]['value_label'].add(know_as)
-        result[source][id_l].append(id_value)
+        result[source][id_l].add(id_value)
 
     # pprint(result)
     return result, property_label
@@ -130,10 +141,13 @@ if __name__ == '__main__':
         w = csv.DictWriter(f, values[0].keys())
         w.writeheader()
         for k, v in result.items():
+            v['value_label'] = list(filter(None, v['value_label']))
             v['value_label'] = list() if not any(v['value_label']) else list(v['value_label'])
-            v['subject_label'] = list(v['subject_label'])
-            v['prop_value'] = list(v['prop_value'])
+            # v['subject_label'] = list(v['subject_label'])
+            # v['prop_value'] = list(v['prop_value'])
+            # v['category'] = list(v['category'])
             for k1, v1 in v.items():
                 if k1 != "source":
+                    # print(k1, v1)
                     v[k1] = sep.join(v1)
             w.writerow(v)
